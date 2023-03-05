073272e94d453d5e9c349863957a4584c6fb77a1
ElkeMilne <126547662+ElkeMilne@users.noreply.github.com>
2023-03-06 00:00:29 +1030

    P

--- main-2-1.cpp
+++ main-2-1.cpp
@@ -3,10 +3,11 @@
 
 extern int min_element(int[], int);
 
-run(){
+int main(){
     int arr1[] = {1, 2, 1, 2, 1};
     int n = sizeof(arr1);
 
     std::cout << min_element(arr1, n) << std::endl;
+    return 0;
 
 }
