11342c5037d996f89eb75391a41fd03d80577d11
ElkeMilne <126547662+ElkeMilne@users.noreply.github.com>
2023-03-05 23:46:48 +1030

    t

--- main-1-5.cpp
+++ main-1-5.cpp
@@ -3,15 +3,7 @@
 
 main(){
     int n1 = 5;
-    int n2 = 2;
-    int n3 = 10;
-    int n4 = 20;
-    int n5 = 0;
 
     std::cout << count_evens(n1) << std::endl;
-    std::cout << count_evens(n2) << std::endl;
-    std::cout << count_evens(n3) << std::endl;
-    std::cout << count_evens(n4) << std::endl;
-    std::cout << count_evens(n5) << std::endl;
 
 }
