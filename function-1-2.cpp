d956ecfa9de75b2d01a61fcd1c18abc05dcd72b7
ElkeMilne <126547662+ElkeMilne@users.noreply.github.com>
2023-03-10 13:17:36 +1030

    p

--- function-1-2.cpp
+++ function-1-2.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 
-int is_identity(int array[2][2]) {
-for (int i = 0; i < 2; i++) {
-    for (int j = 0; j < 2; j++){
+int is_identity(int array[10][10]) {
+for (int i = 0; i < 10; i++) {
+    for (int j = 0; j < 10; j++){
         if (i == j){
              if (array[i][j] != 1) {
                 return false;
