02b746a3a7263aa6b05e232367901374793b0663
ElkeMilne <126547662+ElkeMilne@users.noreply.github.com>
2023-03-16 13:20:40 +1030

    p

--- function-1-1.cpp
+++ function-1-1.cpp
@@ -1,13 +1,10 @@
 #include <iostream>
 
-void print_matrix(int array[10][10]){
-    for (int i = 0; i < 10; i++){
-        for(int j = 0; j < 10; j++){
-            array[i][j] = array[i][j], " "; 
-            if (array[i][i] = 10) {
-                std::cout << std::endl;
-            }
+void print_matrix(int arr[10][10]) {
+    for(int i = 0; i < 10; i++) {
+        for(int j = 0; j < 10; j++) {
+            std::cout << arr[i][j] << " ";
         }
+        std::cout << std::endl;
     }
-    return array;
 }
