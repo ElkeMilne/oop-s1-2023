614ed177cb2b2f0008f78c757e0b63e5044533c8
ElkeMilne <126547662+ElkeMilne@users.noreply.github.com>
2023-03-16 13:36:43 +1030

    p

--- main-2-3.cpp
+++ main-2-3.cpp
@@ -2,6 +2,8 @@
 extern bool is_array_palindrome(int integers[], int length);
 extern int palindrome_sum(int integers[], int length);
 int main(){
+    int integers[] = {1, 2, 3, 2, 1};
+    int length = 5;
     std::cout << is_array_palindrome << std::endl;
     std::cout << palindrome_sum << std::endl;
 }
