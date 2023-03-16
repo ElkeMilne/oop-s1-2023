368dcde8348aebbdda299dbc9f574d53a3d07731
ElkeMilne <126547662+ElkeMilne@users.noreply.github.com>
2023-03-16 13:33:29 +1030

    p

--- function-2-3.cpp
+++ function-2-3.cpp
@@ -1,14 +1,33 @@
 #include <iostream>
 
-int palindrome_sum(int integers[], int length){
-    
+bool palindrome_sum(int integers[], int length) {
+
+    if (length <= 0) {
+        return false;
+    }
+    int left = 0;
+    int right = length - 1;
+    while (left < right) {
+        if (integers[left] != integers[right]) {
+            return false;
+        }
+        left++;
+        right--;
+    }
+    return true;
 }
 
-bool is_array_palindrome(int integers[], int length){
-
-}
-
-int sum_integers(int integers[], int length){
-
+int is_array_palindrome(int integers[], int length) {
+    if (length <= 0) {
+        return -1;
+    }
+    if (palindrome_sum(integers, length)) {
+        int sum = 0;
+        for (int i = 0; i < length; i++) {
+            sum += integers[i];
+        }
+        return sum;
+    }
+    return -2;
 }
 
---
+++ main-2-3.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+extern bool is_array_palindrome(int integers[], int length);
+extern int palindrome_sum(int integers[], int length);
+int main(){
+    std::cout << is_array_palindrome << std::endl;
+    std::cout << palindrome_sum << std::endl;
+}
