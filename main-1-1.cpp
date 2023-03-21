3935d0ae530e9d2a1b0c9d8d01c75e134aac4ae7
ElkeMilne <126547662+ElkeMilne@users.noreply.github.com>
2023-03-21 15:17:43 +1030

    p

--- main-1-1.cpp
+++ main-1-1.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 
-extern int* readNumbers();
-extern void printNumbers(int* numbers, int length);
+//extern int* readNumbers();
+//extern void printNumbers(int* numbers, int length);
+
+#include "function-1-1.cpp"
+
+
 
 int main() {
-    
+    int *array = readNumbers();
+    printNumbers(array, 10);
 }
