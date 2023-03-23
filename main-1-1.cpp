97e977e7fa6aed1568fb0b3bd60ff550a869c636
ElkeMilne <126547662+ElkeMilne@users.noreply.github.com>
2023-03-23 13:49:23 +1030

    p

--- main-1-1.cpp
+++ main-1-1.cpp
@@ -4,7 +4,8 @@
 
 int main() {
     int n = 3;
-    Person* arr = createPersonArray(n);
+    Person* arr;
+    arr = createPersonArray(n);
     for (int i = 0; i < n; i++){
         std::cout << "Person " << i+1 << "- Name: " << arr[i].name << "- Age: " << arr[i].age << "\n";
     }
