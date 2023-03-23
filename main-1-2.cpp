dd89bdb92a48dc0647e666a5debf52a85ddc5b25
ElkeMilne <126547662+ElkeMilne@users.noreply.github.com>
2023-03-23 19:20:04 +1030

    p

---
+++ main-1-2.cpp
@@ -0,0 +1,14 @@
+#include "Person.h"
+#include <iostream>
+extern "function-1-2.cpp"
+
+int main() {
+    int n = 3;
+    PersonList list;
+    list = createPersonList(n);
+    for (int i = 0; i < n; i++) {
+        std::cout << "Person " << i+1 << "- Name: " << list.people[i].name << "- Age: " << list.people[i].age << "\n";
+    }
+    delete[] list.people;
+    return 0;
+}
