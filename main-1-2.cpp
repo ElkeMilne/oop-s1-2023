f9fcd6426a2f388648f8ec06f344fcd0b8e5d12b
ElkeMilne <126547662+ElkeMilne@users.noreply.github.com>
2023-04-09 22:37:37 +0930

    Add files via upload

--- main-1-2.cpp
+++ main-1-2.cpp
@@ -1,45 +1,28 @@
 #include <iostream>
 #include "ParkingLot.h"
-#include "Vehicle.h"
-
-using namespace std;
 
 int main() {
     ParkingLot lot(10);
-
-    // prompt user for vehicle type and park until lot is full
-    while (lot.getCount() < lot.getCapacity()) {
-        char type;
-        cout << "Enter vehicle type (c for car, b for bus, t for truck): ";
-        cin >> type;
-
-        Vehicle* vehicle;
-        switch (type) {
-            case 'c':
-                vehicle = new Car();
-                break;
-            case 'b':
-                vehicle = new Bus();
-                break;
-            case 't':
-                vehicle = new Truck();
-                break;
-            default:
-                cout << "Invalid vehicle type" << endl;
-                continue;
+    int id;
+    std::string type;
+    while (lot.getCount() < 10) {
+        std::cout << "Enter vehicle type (car, truck, motorcycle): ";
+        std::cin >> type;
+        if (type == "car") {
+            Car* car = new Car();
+            lot.parkVehicle(car);
+        } else if (type == "truck") {
+            Truck* truck = new Truck();
+            lot.parkVehicle(truck);
+        } else if (type == "motorcycle") {
+            Motorcycle* motorcycle = new Motorcycle();
+            lot.parkVehicle(motorcycle);
+        } else {
+            std::cout << "Invalid vehicle type" << std::endl;
         }
-
-        lot.parkVehicle(vehicle);
-        cout << "Vehicle parked. Current count: " << lot.getCount() << endl;
     }
-
-    // prompt user for vehicle ID to unpark
-    int vehicle_id;
-    cout << "Enter vehicle ID to unpark: ";
-    cin >> vehicle_id;
-
-    lot.unparkVehicle(vehicle_id);
-    cout << "Vehicle unparked. Current count: " << lot.getCount() << endl;
-
+    std::cout << "Enter ID of vehicle to unpark: ";
+    std::cin >> id;
+    lot.unparkVehicle(id);
     return 0;
 }
